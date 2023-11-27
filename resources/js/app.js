b64c030a31947e2728ec09843ad1a7ee8ea83437
anna <anna9ne@gmail.com>
2023-11-27 14:22:42 +0300

    5-6 Подключение vue js. Первый компонент

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
+import Vue from 'vue'
+import PostComponent from "./components/PostComponent.vue"
+
 require('./bootstrap');
+
+const app = new Vue({
+    el: '#app',
+
+    components: {
+        PostComponent,
+    }
+})
